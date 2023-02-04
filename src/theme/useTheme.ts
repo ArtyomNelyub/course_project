ab5d52d8c317b96a42a3f7802eb3a9224e445c93
Artyom Nelyub <artyom.nelyub.an@gmail.com>
2023-02-04 09:59:59 +0400

    initial commit

---
+++ src/theme/useTheme.ts
@@ -0,0 +1,17 @@
+import { useContext } from 'react';
+import { LOCAL_STORAGE_THEME_KEY, THEME, ThemeContext } from './theme-context';
+
+interface UseThemeResult {
+  toggleTheme: () => void;
+  theme: THEME;
+}
+
+export function useTheme(): UseThemeResult {
+  const { theme, setTheme } = useContext(ThemeContext);
+  const toggleTheme = () => {
+    const newTheme = theme === THEME.LIGHT ? THEME.DARK : THEME.LIGHT
+    setTheme(newTheme);
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
+  };
+  return { theme, toggleTheme };
+}
