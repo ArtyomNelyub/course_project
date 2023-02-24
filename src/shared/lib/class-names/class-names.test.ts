ea9e51dbc8c7aa227a04d06ee50c850ce2e46587
Artyom Nelyub <artyom.nelyub.an@gmail.com>
2023-02-24 10:45:35 +0400

    add jest

---
+++ src/shared/lib/class-names/class-names.test.ts
@@ -0,0 +1,39 @@
+import { classNames } from './class-names';
+
+describe('test classNames function', () => {
+  test('with only first param', () => {
+    expect(classNames('someClass')).toBe('someClass');
+  });
+
+  test('with additional class', () => {
+    const expected = 'cl cl1 cl2';
+    expect(classNames('cl', {}, ['cl1', 'cl2'])).toBe(expected);
+  });
+
+  test('with mods', () => {
+    const expected = 'cl cl1 cl2 cl3 cl4 cl5';
+    expect(classNames('cl', {
+      cl3: true,
+      cl4: true,
+      cl5: true,
+    }, ['cl1', 'cl2'])).toBe(expected);
+  });
+
+  test('with mods false', () => {
+    const expected = 'cl cl1 cl2 cl3 cl4';
+    expect(classNames('cl', {
+      cl3: true,
+      cl4: true,
+      cl5: false,
+    }, ['cl1', 'cl2'])).toBe(expected);
+  });
+
+  test('with mods undefined', () => {
+    const expected = 'cl cl1 cl2 cl3 cl4';
+    expect(classNames('cl', {
+      cl3: true,
+      cl4: true,
+      cl5: undefined,
+    }, ['cl1', 'cl2'])).toBe(expected);
+  });
+});
