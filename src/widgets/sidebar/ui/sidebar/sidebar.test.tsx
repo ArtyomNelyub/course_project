db93956ac7850d6b44f015ff107456694fd3fcc4
Artyom Nelyub <artyom.nelyub.an@gmail.com>
2023-03-06 20:08:38 +0400

    fix tests

--- src/widgets/sidebar/ui/sidebar/sidebar.test.tsx
+++ src/widgets/sidebar/ui/sidebar/sidebar.test.tsx
@@ -1,17 +1,17 @@
 import { fireEvent, screen } from '@testing-library/react';
 import {
-  renderWithTranslation,
-} from 'shared/lib/tests/render-with-translation/render-with-translation';
+  componentRender,
+} from 'shared/lib/tests/component-render/component-render';
 import { Sidebar } from 'widgets/sidebar/ui/sidebar/sidebar';
 
 describe('Testing sidebar', () => {
   test('test render', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('test toggle', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     fireEvent.click(toggleBtn);
