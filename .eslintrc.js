ab36572daa8031b1c4ea75bcf2fbaa6cfec35d1b
Artyom Nelyub <artyom.nelyub.an@gmail.com>
2023-03-07 15:29:00 +0400

    last try

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,10 +38,7 @@ module.exports = {
     'no-unused-vars': 'warn',
     'no-shadow': 'off',
     'no-underscore-dangle': 'off',
-    'linebreak-style': [
-      'error',
-      process.platform === 'win32' ? 'windows' : 'unix',
-    ],
+    'linebreak-style': 0,
     'i18next/no-literal-string': [
       'error',
       {
